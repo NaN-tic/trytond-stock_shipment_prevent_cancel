983ae7eb4a84ad3094859c5dcefd4b3e98a4a66f
Albert Cervera i Areny <albert@nan-tic.com>
2018-05-31 15:50:37 +0200

    Do not test views because there are none.

--- tests/test_stock_shipment_prevent_cancel.py
+++ tests/test_stock_shipment_prevent_cancel.py
@@ -3,9 +3,7 @@
 import unittest
 # import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import test_depends
 
 
 class TestCase(unittest.TestCase):
@@ -13,10 +11,6 @@ class TestCase(unittest.TestCase):
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_shipment_prevent_cancel')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_prevent_cancel')
 
     def test0006depends(self):
         'Test depends'
@@ -26,8 +20,4 @@ class TestCase(unittest.TestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_stock_shipment_prevent_cancel.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
