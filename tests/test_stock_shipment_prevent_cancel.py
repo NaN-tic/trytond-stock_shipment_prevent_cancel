852a77eccd0d2131d26f852b1f20a5f9ac366d5d
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-28 12:30:02 +0100

    Upgrade to Tryton 4.8

--- tests/test_stock_shipment_prevent_cancel.py
+++ tests/test_stock_shipment_prevent_cancel.py
@@ -1,33 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-# import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_prevent_cancel')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_prevent_cancel')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
+class StockShipmentPreventCancelTestCase(ModuleTestCase):
+    'Test Stock Shipment Prevent Cancel module'
+    module = 'stock_shipment_prevent_cancel'
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_stock_shipment_prevent_cancel.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            StockShipmentPreventCancelTestCase))
     return suite
